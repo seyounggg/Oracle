2226fc1f7b5406421519b7534c35c7c2e040a5ac
SIST <SIST@DESKTOP-CPMDO9H>
2023-05-17 18:07:53 +0900

    2023_05_16_오라클 연동

---
+++ OracleConnectionProject/src/com/sist/main/MainClass.java
@@ -0,0 +1,18 @@
+package com.sist.main;
+import java.util.*;
+import com.sist.dao.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		EmpDAO dao=new EmpDAO();
+		List<EmpVO> list=dao.empListDate();
+		for(EmpVO vo:list) {
+			System.out.println(vo.getEmpno()+""
+						+vo.getEname()+""
+						+vo.getJob()+""
+						+vo.getSal());
+		}
+		
+	}
+
+}
